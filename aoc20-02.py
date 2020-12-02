8a02184027abcad6c664a160e8483b8bf1aad262
YahiaWL <yahiaworklaptop@gmail.com>
2020-12-02 08:27:27 +0000

    Update aoc20-02.py

--- aoc20-02.py
+++ aoc20-02.py
@@ -1,6 +1,6 @@
 valid_pass = 0
-for password in r_inp_full:
-    nums, letter, pwd = password.split(' ')
+for inp in r_inp_full:
+    nums, letter, pwd = inp.split(' ')
     n_min, n_max = [int(_) for _ in nums.split('-')]
     if n_min <= pwd.count(letter[0]) <= n_max:
         valid_pass += 1
@@ -12,10 +12,10 @@ valid_pass
 # Part 2
 
 valid_pass = 0
-for password in r_inp_full:
-    nums, letter, pwd = password.split(' ')
-    n_1, n_2 = [int(_) for _ in nums.split('-')]
-    if (pwd[n_1-1] == letter[0]) + (pwd[n_2-1] == letter[0]) == 1 : 
+for inp in r_inp_full:
+    nums, letter, pwd = inp.split(' ')
+    i_1, i_2 = (int(_)-1 for _ in nums.split('-'))
+    if (pwd[i_1] == letter[0]) + (pwd[i_2] == letter[0]) == 1 : 
         valid_pass += 1
         
 valid_pass
